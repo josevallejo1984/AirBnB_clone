c9177df69eb19ab10c14f78440b31a365e5757f3
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-26 12:10:27 -0500

    update rep

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Create class BaseModel that defines all common 
-attributes/methods for other class
+   attributes/methods for other class
 """
 import models
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel(object):
     """Define class BaseModel"""
+
     def __init__(self):
         """Initialized constructor"""
-        time_form = "%Y-%m-%dT%H:%M:%S.%f"    
+        time_form = "%Y-%m-%dT%H:%M:%S.%f"
         self.updated_at = datetime.today()
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -25,15 +27,13 @@ class BaseModel(object):
         __dict__ of the instance
         """
         new_dict = self.__dict__.copy()
-        new_dict.update({'__class__': self.__class__.__name__})
         new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["created_at"] = self.created_at.isoformat()
-        #new_dict["__class__"] = self.__class__.__name__
-        
+        new_dict["__class__"] = self.__class__.__name__
+
         return new_dict
 
     def __str__(self):
         """print string"""
         class_name = self.__class__.__name__
         return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
-
