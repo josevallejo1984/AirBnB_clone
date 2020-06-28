6d48ef8634ba628b956c5c59c4ced49d81f41386
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-06-28 07:47:47 -0500

    task 5 work all done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-""" Serializes instances to a JSON file and
-deserializes JSON file to instances
+"""Serializes instances to a JSON file and.
+   deserializes JSON file to instances
 """
-import models
-from uuid import uuid4
-from datetime import datetime
 import json
 
 
 class FileStorage(object):
-    """Define class FileStorage"""
+    """Define class FileStorage."""
 
-    def __init__(self):
-        """Initialized constructor
-        """
-        self.__file_path = "file.json"
-        self.__objects = {}
-        self.__key = ""
-        self.__object = None
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
-        self.__object = obj
-        id_obj = str(obj.__dict__.get('id'))
-        name = str(obj.__class__.__name__)
-        self.__key = name + '.' + id_obj
-        self.__objects[self.__key] = obj.__dict__
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        self.__objects[self.__key] = self.__object.to_dict()
+        new = {}
         with open(self.__file_path, mode='w', encoding='utf-8') as my_file:
-            my_file.write(json.dumps(self.__objects))
+            for k, v in FileStorage.__objects.items():
+                new.update({k: v.to_dict()})
+            my_file.write(json.dumps(new))
 
     def reload(self):
         try:
             with open(self.__file_path, mode='r', encoding='utf-8') as my_file:
-                self.__objects = json.loads(my_file.read())
-        except:
+                from models.base_model import BaseModel
+                new_dict = json.loads(my_file.read())
+                for k, v in new_dict.items():
+                    objt = BaseModel(**v)
+                    FileStorage.__objects[k] = objt
+
+        except IOError:
             pass
