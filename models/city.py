4b0d97ed6230b9690cb1e1b6f03dde91c82b3fcd
cbarros7 <cj.barros07@gmail.com>
2020-07-01 03:29:32 +0000

    Fix pep8 for files models

--- models/city.py
+++ models/city.py
@@ -5,5 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """Class City that inherit from BaseModel"""
+
     state_id = ""
     name = ""
--- models/review.py
+++ models/review.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """Class Review that inherit from BaseModel"""
+
     place_id = ""
     user_id = ""
     text = ""
